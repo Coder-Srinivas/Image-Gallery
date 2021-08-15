6dfd6916544da62ac2c7f3df31ce3870e27765a9
Coder-Srinivas <srinivas.babu364@gmail.com>
2021-08-15 17:30:09 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,29 @@
+const url = '/data.json';
+
+const main = document.getElementById("main");
+fetch(url).then(response => response.json()).then(data => displayImages(data));
+
+const displayImages = (data) => {
+    data.forEach((element) => {
+        // Creating a block
+        const div = document.createElement("div");
+        div.classList.add("image-block");
+    
+        // Creating an image for the block
+        const image = document.createElement("img");
+        image.src = element.image;
+        image.classList.add("image");
+    
+        // Creating a title for the block
+        const title = document.createElement("div");
+        title.innerHTML = element.title;
+        title.classList.add("title");
+
+        // Appending it to the div
+        div.appendChild(image);
+        div.appendChild(title);
+    
+        // Appending to the main div
+        main.appendChild(div);
+    })
+}
